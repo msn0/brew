cf46205896b16dfa237e5945f52a1738e63f8baf
Michał Jezierski <ambinanitelo@gmail.com>
2019-05-30 14:23:46 +0200

    cleanup

--- src/components/app/index.js
+++ src/components/app/index.js
@@ -1,18 +1,16 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import styles from './styles.module.css';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
+      <div className={ styles.wrapper }>
+        <header className={ styles.header }>
           <p>
             Edit <code>src/App.js</code> and save to reload.
           </p>
           <a
-            className="App-link"
+            className={ styles.link }
             href="https://reactjs.org"
             target="_blank"
             rel="noopener noreferrer"
