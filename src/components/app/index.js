5b37d5ff6559107461eeefdceebdebd591432620
Michał Jezierski <ambinanitelo@gmail.com>
2019-05-30 15:32:22 +0200

    heading

--- src/components/app/index.js
+++ src/components/app/index.js
@@ -6,17 +6,12 @@ class App extends PureComponent {
     return (
       <div className={ styles.wrapper }>
         <header className={ styles.header }>
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className={ styles.link }
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
+          <h1>
+            brew<sup>2</sup>
+          </h1>
+          <h2>
+            The Home Brew Shop
+          </h2>
         </header>
       </div>
     );
