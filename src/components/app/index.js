e8c79c2ad5ecc4e656a1502e5c7fdde994a8f448
Michał Jezierski <ambinanitelo@gmail.com>
2019-05-30 15:41:33 +0200

    Navigation

--- src/components/app/index.js
+++ src/components/app/index.js
@@ -1,10 +1,14 @@
 import React, { PureComponent } from 'react';
 import styles from './styles.module.css';
+import { Navigation } from '../navigation';
 
 class App extends PureComponent {
   render() {
     return (
       <div className={ styles.wrapper }>
+        <div className={ styles.navWrapper }>
+          <Navigation />
+        </div>
         <header className={ styles.header }>
           <h1>
             brew<sup>2</sup>
---
+++ src/components/navigation/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import styles from './styles.module.css';
+
+export function Navigation() {
+    return (
+        <nav className={ styles.wrapper }>
+            start
+        </nav>
+    );
+}
