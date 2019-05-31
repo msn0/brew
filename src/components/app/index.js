db77bd396dc67eb6f99f2ca015a806a9ade4da86
Michał Jezierski <ambinanitelo@gmail.com>
2019-05-31 21:09:52 +0200

    images

--- src/components/app/index.js
+++ src/components/app/index.js
@@ -1,13 +1,16 @@
-import React, { PureComponent } from 'react';
+import React from 'react';
 import styles from './styles.module.css';
 import { Navigation } from '../navigation';
 import { Product } from '../product';
+import slod1 from './assets/slod1.jpg';
+import slod2 from './assets/slod2.jpg';
+import slod3 from './assets/slod3.jpg';
 
 const products = [
   {
     name: 'Wheat Malt - Malteurop',
     brand: 'Malteurop',
-    image: '/assets/slod1.jpg',
+    image: slod1,
     price: {
       amount: 6,
       currency: 'PLN'
@@ -16,7 +19,7 @@ const products = [
   {
     name: 'Barley Malt',
     brand: 'Viking Malt',
-    image: '/assets/slod2.jpg',
+    image: slod2,
     price: {
       amount: 5,
       currency: 'PLN'
@@ -25,7 +28,7 @@ const products = [
   {
     name: 'Wheat Malt',
     brand: 'Viking Malt',
-    image: '/assets/slod3.jpg',
+    image: slod3,
     price: {
       amount: 6,
       currency: 'PLN'
@@ -33,27 +36,23 @@ const products = [
   }
 ];
 
-class App extends PureComponent {
-  render() {
-    return (
-      <div className={ styles.wrapper }>
-        <Navigation />
-        <header className={ styles.header }>
-          <h1>
-            brew<sup>2</sup>
-          </h1>
-          <h2>
-            The Home Brew Shop
+export default function App() {
+  return (
+    <div className={styles.wrapper}>
+      <Navigation />
+      <header className={styles.header}>
+        <h1>
+          brew<sup>2</sup>
+        </h1>
+        <h2>
+          The Home Brew Shop
           </h2>
-        </header>
-        <main>
-          { products.map(product => 
-            <Product product={ product } />
-          ) }
-        </main>
-      </div>
-    );
-  }
+      </header>
+      <main>
+        {products.map(product =>
+          <Product product={product} />
+        )}
+      </main>
+    </div>
+  );
 }
-
-export default App;
