fa8153428987ed71f2a1b6b02fe7b7340eb3e18f
Michał Jezierski <ambinanitelo@gmail.com>
2019-06-01 19:51:41 +0200

    safbrew wb-06

--- src/dummy/products.js
+++ src/dummy/products.js
@@ -1,15 +1,20 @@
 import slod1 from './assets/slod1.jpg';
 import slod2 from './assets/slod2.jpg';
 import slod3 from './assets/slod3.jpg';
+import wb06 from './assets/wb06.jpg';
 
 export default [
     {
         id: '1',
-        name: 'Wheat Malt - Malteurop',
-        brand: 'Malteurop',
-        image: slod1,
+        name: 'Safbrew WB-06',
+        brand: 'Fermentis',
+        image: wb06,
+        description: 'Specialty yeast selected for wheat beer fermentations. ' +
+            'Produces subtle estery and phenol flavor notes typical of wheat beers. ' +
+            'Allows to brew beer with a high drinkability profile and presents ' +
+            'a very good ability to suspend during fermentation.',
         price: {
-            amount: 6,
+            amount: 13.99,
             currency: 'PLN'
         }
     },
