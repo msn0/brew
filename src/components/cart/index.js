e171979aebf8dfb23cd61a4eaaf09a891f6f3336
Michał Jezierski <ambinanitelo@gmail.com>
2019-06-01 23:05:29 +0200

    cart

--- src/components/cart/index.js
+++ src/components/cart/index.js
@@ -1,21 +1,35 @@
 import React from 'react';
 import { connect } from 'react-redux';
+import * as styles from './styles.module.css';
 
 const byId = (id, products) => products.find(product => product.id === id);
 
-function renderProduct(product) {
+function renderCartProduct(product) {
+    const price = new Intl.NumberFormat('pl-PL', {
+        style: 'currency',
+        currency: product.price.currency
+    }).format(product.price.amount);
+
     return (
-        <li key={ product.id }>{ product.name }</li>
+        <article className={ styles.cartProduct } key={ product.id }>
+            <div className={ styles.left }>
+                <img src={ product.image } alt={ product.name } />
+            </div>
+            <div className={ styles.right }>
+                <h5 className={ styles.name }>{ product.name }</h5>
+                <div className={ styles.price }>
+                    1 x { price }
+                </div>
+            </div>
+        </article>
     );
 }
 
 function Cart ({ cart = [], products = [] }) {
     return (
-        <div>
-            <h4>Twój koszyk</h4>
-            <ul>
-                { cart.map(id => renderProduct(byId(id, products))) }
-            </ul>
+        <div className={ styles.wrapper }>
+            <h4 className={ styles.header }>Twój koszyk</h4>
+            { cart.map(id => renderCartProduct(byId(id, products))) }
         </div>
     );
 }
